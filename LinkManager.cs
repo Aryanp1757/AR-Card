fc20b073879c5ec08dad1463a1e3c92e75887ac7
Aryan Prajapati <95575009+Aryanp1757@users.noreply.github.com>
2023-04-23 18:30:46 +0530

    Scripts

---
+++ LinkManager.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LinkManager : MonoBehaviour
+{
+    public void openlinkedin()
+    {
+        Application.OpenURL("https://www.linkedin.com/in/aryan-prajapati-770429226/");
+    }
+
+    public void openinstagram()
+    {
+        Application.OpenURL("https://www.instagram.com/__.a.r.y.a.n/?hl=en");
+    }
+
+    public void openyoutube()
+    {
+        Application.OpenURL("https://www.youtube.com/channel/UCajjpKuEmfuLYfotd_NvcJA");
+    }
+
+    public void opengithub()
+    {
+        Application.OpenURL("https://github.com/aryan1757");
+    }
+
+    public void openportfolio()
+    {
+        Application.OpenURL("https://aryan-kunalgsolanki-kgs2002107.vercel.app/");
+    }
+}
